38f05f78e1f4563b27ae18b230a8ae26745f512c
Taize Santos <taizeonze@gmail.com>
2022-02-02 20:55:54 -0300

    Inclusão dos arquivos HTML, CSS, lógica de mascara e validador com a manipulação de DOM somente com o validador

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import validator from './validator.js';
 
+const inputNumeroCartao = (document.getElementById("numeroCard"));
+
+
+
+const botaoValidar = document.getElementById("botaoValidar");
+
+botaoValidar.addEventListener("click", validaNumeroCartao);
+
+function validaNumeroCartao(e) {
+  e.preventDefault();
+
+  let numeroCartao = inputNumeroCartao.value.replace();
+
+  const retornaValidacao = validator.isValid(numeroCartao);
+
+  document.getElementById("resultadoValidacao").innerHTML = retornaValidacao
+
+
+}
+
+
+
+
 console.log(validator);
